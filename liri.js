16cf1f14b0e55dc921c18cb4749b1b0d5f69ef05
Braniel J. Pichardo <branieljose@gmail.com>
2017-02-01 18:37:16 -0500

    basic logic

--- liri.js
+++ liri.js
@@ -1,21 +1,57 @@
 let keys = require('./keys');
 let Twitter = require('twitter');
-let spotify = require('spotify');
 let request = require('request');
+let fs = require('fs');
 let command = process.argv[2];
-let a = keys.twitterKeys;	
-var client = new Twitter({
-  consumer_key: 'vyaOg5TMYfHbIxJG19edA9FS4',
-  consumer_secret: 'uzM2Tc15Mbtum9GSPhQ2Z1sFmTtC1Enu8XbJuLUGjEWWlVFx1N',
-  access_token_key: '824408584356327424-EDenyBS1CiXQsN5efiGHstPkYBXDJ8p',
-  access_token_secret: 'aech5gLO0ozT8wINlZNt04ft8KNA3quueC47cd53tzYFO',
-});
- 
-var params = {screen_name: 'nodejs'};
-client.get('search/tweets', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  } else {console.log(error);}
-});
+let input = process.argv[3];
+let a = keys.twitterKeys;
+var client = new Twitter(a);
 
-	console.log(a);
+
+function doit(command, input) {
+
+    switch (command) {
+        case 'my-tweets':
+            var params = {
+                screen_name: 'fakatoon_21'
+            };
+            client.get('statuses/user_timeline', params, function(error, tweets, response) {
+                if (!error) {
+                    for (var i = 0; i < tweets.length; i++) {
+                        console.log('\n' + tweets[i].created_at + ': ' + tweets[i].text);
+                    }
+                } else {
+                    console.log(error);
+                }
+            });
+            break;
+        case 'spotify-this-song':
+            request.get('https://api.spotify.com/v1/search?type=track&q=' + input + '&limit=1', function(error, response) {
+                if (!error && response.statusCode == 200) {
+                    var doc = JSON.parse(response.body);
+                    doc.tracks.items.some(function(data) {
+                        let info = data;
+                        let artistName = info.artists[0].name;
+                        let songName = info.name;
+                        let prev_URL = info.preview_url;
+                        let album = info.album.name;
+                        console.log(`Song: ${songName}\nArtist Name: ${artistName}\nAlbum: ${album}\nPreview URL: ${prev_URL}\n`);
+                    });
+                }
+
+            });
+            break;
+        case '1':
+        fs.readFile('./random.txt', 'UTF8',function(err,data){
+            if (err) {
+                console.log(err);
+            }   let file = data.split(',');
+                let cmd = file[0];
+                let inpt = file[1];
+                doit(cmd, inpt);
+        });
+
+
+    }
+}
+doit(command, input);
