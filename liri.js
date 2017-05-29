eb80a30e8de2294d0dc1c2342f1ca693ed8e2a67
braniel pichardo <branielpichardo@braniels-MacBook-Pro.local>
2017-05-29 18:23:17 -0400

    improved logic, new UI, new API

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ var keys = require('./keys');
 var Twitter = require('twitter');
 var request = require('request');
 var fs = require('fs');
+var inquirer = require('inquirer');
+var Spotify = require('machinepack-spotify');
 
 //collect commands to be executed
 var command = process.argv[2];
@@ -14,108 +16,49 @@ var a = keys.twitterKeys;
 //passes keys on to the Twitter constructor function
 var client = new Twitter(a);
 
+function startInt() {
+    
+    inquirer.prompt([{
+        type: 'list',
+        name: 'options',
+        message: 'What would you like to do?',
+        choices: ['1. View my Tweets', '2. Spotify a Song', '3. Look Up the Weather in Your Area', '4. Do What it Says'],
+        filter: function(val) {
+            return val.toLowerCase();
+        }
+    }]).then(function(answers) {
+        var command = parseInt(answers.options);
+        if (command === 1) {
+            firstCommand();      
+        } else if (command === 2) {
+            // secondCommand();
+            spotify();   
+        } else if (command === 3) {
+            thirdCommand();
+        } else if (command === 4) {
+            fourthCommand();
+        }
+
+    });
+}
+startInt();
+
 //this function contains the switch and cases
 function doIt(command, input) {
 
     switch (command) {
         case 'my-tweets':
-            //makes a request to the twitter API 
-            var params = {
-                screen_name: 'fakatoon_21'
-            };
-            client.get('statuses/user_timeline', params, function(error, tweets, response) {
-                //checks if there is an error, logs tweets if not error found 
-                if (!error) {
-                    for (var i = 0; i < tweets.length; i++) {
-                        console.log('\n' + tweets[i].created_at + ': ' + tweets[i].text);
-                    }
-                }
-                //logs error
-                else {
-                    console.log(error);
-                }
-            });
+
             break;
         case 'spotify-this-song':
-            //checks name of song, pass a default name if any found
-            if (!input) {
-                var defaultSong = 'ace of base the sign';
-                getSong(defaultSong);
-            } else {
-                getSong(input);
-            }
-            //gets song info from API and logs it 
-            function getSong(input) {
-                //requests song info to the API
-                request.get('https://api.spotify.com/v1/search?type=track&q=' + input + '&limit=1', function(error, response) {
-                    if (!error && response.statusCode == 200) {
-                        //parses response
-                        var doc = JSON.parse(response.body);
-                        //logs data 
-                        doc.tracks.items.some(function(data) {
-                            var info = data;
-                            var artistName = info.artists[0].name;
-                            var songName = info.name;
-                            var prev_URL = info.preview_url;
-                            var album = info.album.name;
-                            console.log('Song: ' + songName +
-                                '\nArtist Name: ' + artistName +
-                                '\nAlbum: ' + album +
-                                '\nPreview URL: ' + prev_URL
-                            );
-                        });
-                    }
-
-                });
-            }
 
             break;
         case 'movie-this':
             //here I repeat the same steps again
-            if (!input) {
-                var defaultMovie = 'Mr. nobody';
-                getMovie(defaultMovie);
-            } else {
-                getMovie(input);
-            }
 
-            function getMovie(input) {
-                request.get('http://www.omdbapi.com/?t=' + input + '&y=&plot=short&tomatoes=true&r=json', function(err, res) {
-                    if (!err && res.statusCode == 200) {
-                        var doc = JSON.parse(res.body);
-                        console.log('Title: ' + doc.Title +
-                            '\nYear: ' + doc.Year +
-                            '\nimdbRating: ' + doc.imdbRating +
-                            '\nCountry: ' + doc.Country +
-                            '\nLanguage: ' + doc.Language +
-                            '\nPlot: ' + doc.Plot +
-                            '\nActors: ' + doc.Actors +
-                            '\nRotten Tomatoes: ' + doc.tomatoRotten +
-                            '\nRotten Tomatoes URL: ' + doc.tomatoURL
-                        );
-                    } else {
-                        console.log(err);
-                    }
-
-                });
-            }
             break;
         case 'do-what-it-says':
-            //reads .txt file 
-            fs.readFile('./random.txt', 'UTF8', function(err, data) {
-                //logs error, if there is any 
-                if (err) {
-                    console.log(err);
-                }
-                //items separated by commas will become keys inside an array
-                var file = data.split(',');
 
-                //store each key separately 
-                var cmd = file[0];
-                var inpt = file[1];
-                //pass keys as params and runs function 
-                doIt(cmd, inpt);
-            });
 
 
     }
@@ -125,5 +68,135 @@ function doIt(command, input) {
         if (err) throw err;
     });
 }
-//calling main function
-doIt(command, input);
+
+function firstCommand(){
+//my-tweets
+//makes a request to the twitter API 
+    var params = {
+        screen_name: 'fakatoon_21'
+    };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        //checks if there is an error, logs tweets if not error found 
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log('\n > ' + tweets[i].created_at + ': ' + tweets[i].text);
+            }
+            goBack();
+        }
+        //logs error
+        else {
+            console.log(error);
+        }
+    });
+}
+
+function secondCommand(){
+//spotify-this-song
+
+    inquirer.prompt([{
+        type: 'input',
+        name: 'songName',
+        message: 'What\'s the name of the song?',
+        filter: function(val) {
+            return val.toLowerCase();
+        }
+    }]).then(function(answers) {
+        var command = answers.songName;
+        /**gets song info from API and logs it 
+            requests song info to the API **/
+        var input = command || "ace of base the sign";
+        request.get('https://api.spotify.com/v1/search?type=track&q=' + input + '&limit=1', function(error, response) {
+            if (!error && response.statusCode == 200) {
+                //parses response
+                var doc = JSON.parse(response.body);
+                //logs data 
+                doc.tracks.items.some(function(data) {
+                    var info = data;
+                    var artistName = info.artists[0].name;
+                    var songName = info.name;
+                    var prev_URL = info.preview_url;
+                    var album = info.album.name;
+                    console.log('Song: ' + songName +
+                        '\nArtist Name: ' + artistName +
+                        '\nAlbum: ' + album +
+                        '\nPreview URL: ' + prev_URL
+                    );
+                    goBack();
+                });
+            }
+
+        });
+
+    });
+
+}
+
+function thirdCommand(input){
+//weather-this
+ inquirer.prompt([{
+    type: 'input',
+    name: 'zip',
+    message: 'What\'s your zipcode?',
+    filter: function(val) {
+        return val.toLowerCase();
+    } 
+    }]).then(function(answers) {
+        var command = answers.zip,
+            input = command || "07003";
+            
+            request.get("http://api.openweathermap.org/data/2.5/weather?zip="+ input +",us&APPID=bf16f4332356ef842aea73a080ad5791", function(err, res) {
+            if (!err && res.statusCode == 200) {
+                
+                var doc = JSON.parse(res.body),
+                    //converts temperature from Kalvin to Fahrenheit
+                    minTemp = Math.round(9/5 * (doc.main.temp_min - 273) + 32),
+                    maxTemp = Math.round(9/5 * (doc.main.temp_max - 273) + 32);
+                
+
+                console.log('Description: ' + doc.weather[0].description  + '\n' + 
+                            'Location: '    + doc.name                    + '\n' +
+                            "Temperature: "      + 
+                            "Min. "  + minTemp + '°F' + ', ' +
+                            " Max. " + maxTemp + '°F'                     + '\n' +
+                            "Wind: " + doc.wind.speed + 'm/s' );
+
+            } else {
+                console.log(err);
+            }
+            goBack();
+    });
+})
+
+}
+
+function fourthCommand(){
+//do-what-it-says
+//reads .txt file 
+    fs.readFile('./random.txt', 'UTF8', function(err, data) {
+        //logs error, if there is any 
+        if (err) {
+            console.log(err);
+        }
+        //items separated by commas will become keys inside an array
+        var file = data.split(',');
+
+        //store each key separately 
+        var cmd = file[0];
+        var inpt = file[1];
+        //pass keys as params and runs function 
+        // doIt(cmd, inpt);
+    });
+}
+
+function goBack(){
+     inquirer.prompt([{
+        type: 'confirm',
+        name: 'songName',
+        message: 'Go back',
+        filter: function(val) {
+            return val.toLowerCase();
+        }
+    }]).then(function(answers) {
+        startInt();
+    })
+}
